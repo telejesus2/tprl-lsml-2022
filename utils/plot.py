cc2fecb45c1bc1168683e759da34c1cd7fc7ac05
telejesus2 <jebumagoma@gmail.com>
2022-02-18 12:10:25 +0100

    fix bug

--- utils/plot.py
+++ utils/plot.py
@@ -88,7 +88,7 @@ def get_datasets(fpath, condition=None):
     return datasets
 
 
-def plot(logdir, legend=None, value=None, x=None):
+def plot(logdir, legend=None, value=None, x="Iteration"):
     use_legend = False
     if legend is not None:
         assert len(legend) == len(logdir), \
